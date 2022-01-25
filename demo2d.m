721f6021c78993971662e182685b2fbfb36aecde
Abderahmane BENALI <46283596+benaliabderrahmane@users.noreply.github.com>
2022-01-25 13:36:33 +0100

    Add files via upload

---
+++ demo2d.m
@@ -0,0 +1,14 @@
+clear;
+x2 = sin(linspace(0,2*pi));
+y2 = cos(linspace(0,3*pi));
+X = [x2',y2'];
+[L2,R2,K2] = curvature(X);
+figure;
+h = plot(x2,y2); grid on; axis equal
+set(h,'marker','.');
+xlabel x
+ylabel y
+title('2D curve with curvature vectors')
+hold on
+quiver(x2',y2',K2(:,1),K2(:,2));
+hold off
